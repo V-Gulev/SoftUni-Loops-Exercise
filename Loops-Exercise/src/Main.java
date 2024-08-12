99a0fcd7072dcd3f856fcb62e58e907ceb8a4e1d
V-Gulev <velizargulev@abv.bg>
2024-08-12 20:54:04 +0300

    Triangle of Numbers

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -3,69 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double sum = 0;
-        double moneyDouble;
-        while (true) {
-            String money = sc.nextLine();
-            if (money.equals("Start")) {
-                break;
+        int number = Integer.parseInt(sc.nextLine());
+        for (int i = 1; i <=number ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(i+" ");
             }
-            switch (money) {
-                case "0.1", "0.2", "0.5", "1", "2":
-                    moneyDouble = Double.parseDouble(money);
-                    sum += moneyDouble;
-                    break;
-                default:
-                    moneyDouble = Double.parseDouble(money);
-                    System.out.printf("Cannot accept %.2f", moneyDouble);
-                    System.out.println(" ");
-                    break;
-            }
+            System.out.println();
         }
-        while (true) {
-            String food = sc.nextLine();
-            if (food.equals("End")){
-                System.out.printf("Change: %.2f",sum);
-                break;
-            }
-            switch (food) {
-                case "Nuts":
-                    if (sum >= 2) {
-                        System.out.println("Purchased Nuts");
-                        sum-=2;
-                    } else System.out.println("Sorry, not enough money");
-                    break;
-                case "Water":
-                    if (sum >= 0.7) {
-                        System.out.println("Purchased Water");
-                        sum-=0.7;
-                    } else System.out.println("Sorry, not enough money");
-                    break;
-                case "Soda":
-                    if (sum >= 0.8) {
-                        System.out.println("Purchased Soda");
-                        sum-=0.8;
-                    } else System.out.println("Sorry, not enough money");
-                    break;
-                case "Coke":
-                    if (sum >= 1) {
-                        System.out.println("Purchased Coke");
-                        sum-=1;
-                    } else System.out.println("Sorry, not enough money");
-                    break;
-                case "Crisps":
-                    if (sum >= 1.5) {
-                        System.out.println("Purchased Crisps");
-                        sum-=1.5;
-                    } else System.out.println("Sorry, not enough money");
-                    break;
-                default:
-                    System.out.println("Invalid product");
-                    break;
-            }
 
 
-        }
+
     }
 }
 
