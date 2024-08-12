ac2acb783c1ef74eb859fdd87fa14dd5bf88af8e
V-Gulev <velizargulev@abv.bg>
2024-08-12 19:30:28 +0300

    Student Information

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scannerInput = new Scanner(System.in);
+        String name = scannerInput.nextLine();
+        int age = Integer.parseInt(scannerInput.nextLine());
+        double grade = Double.parseDouble(scannerInput.nextLine());
+        System.out.printf("Name: %s, Age: %d, Grade: %.2f",name,age,grade);
+    }
 }
