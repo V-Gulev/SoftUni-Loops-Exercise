a35c3fa4a5f1eb32fc1000918cf329dd4a8e6f2e
V-Gulev <velizargulev@abv.bg>
2024-08-12 20:46:18 +0300

    Print and Sum

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -1,72 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int people = Integer.parseInt(scanner.nextLine());
-        String typeOfPeople = scanner.nextLine();
-        String day = scanner.nextLine();
-        double totalCost = 0;
-        switch (typeOfPeople) {
-            case "Students":
-                switch (day) {
-                    case "Friday":
-                        totalCost = people * 8.45;
-                        break;
-                    case "Saturday":
-                        totalCost = people * 9.80;
-                        break;
-                    case "Sunday":
-                        totalCost = people * 10.46;
-                        break;
-                    default:
-                        break;
-                }
-                if (people >= 30) {
-                    totalCost *= 0.85;
-                }
-                break;
-            case "Business":
-                if (people >= 100) {
-                    people -= 10;
-                }
-                switch (day) {
-                    case "Friday":
-                        totalCost = people * 10.90;
-                        break;
-                    case "Saturday":
-                        totalCost = people * 15.60;
-                        break;
-                    case "Sunday":
-                        totalCost = people * 16.00;
-                        break;
-                    default:
-                        break;
-                }
-                break;
-            case "Regular":
-                switch (day) {
-                    case "Friday":
-                        totalCost = people * 15.00;
-                        break;
-                    case "Saturday":
-                        totalCost = people * 20.00;
-                        break;
-                    case "Sunday":
-                        totalCost = people * 22.50;
-                        break;
-                    default:
-                        break;
-                }
-                if (people >= 10 && people <= 20) {
-                    totalCost *= 0.95;
-                }
-                break;
-            default:
-                break;
+        Scanner sc = new Scanner(System.in);
+        int number1 = Integer.parseInt(sc.nextLine());
+        int number2 = Integer.parseInt(sc.nextLine());
+        int sum = 0;
+        for (int i = number1; i <= number2; i++) {
+            System.out.print(i + " ");
+            sum += i;
         }
-        System.out.printf("Total price: %.2f",totalCost);
+        System.out.println();
+        System.out.println("Sum: " + sum);
 
     }
 }
+
+
