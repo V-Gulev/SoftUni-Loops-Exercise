0010e99d922db309342624f0ab269c42888db2c2
V-Gulev <velizargulev@abv.bg>
2024-08-12 20:24:33 +0300

    Time after 30 Minutes

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scannerInput = new Scanner(System.in);
-        double grade = Double.parseDouble(scannerInput.nextLine());
-        if (grade>=3.00){
-            System.out.println("Passed!");
-        }else System.out.println("Failed!");
+        Scanner sc = new Scanner(System.in);
+        int n1 = Integer.parseInt(sc.nextLine());
+        int n2= Integer.parseInt(sc.nextLine());
+        int n3=n2+30;
+        if (n3>=60){
+            n1= n1+1;
+            n3=n3-60;
+        }
+        if (n1 == 24) {
+            n1=0;
+        }
+        if (n3<10){
+            System.out.printf("%d:0%d",n1,n3);
+        }else System.out.printf("%d:%d",n1,n3);
+
     }
 }
