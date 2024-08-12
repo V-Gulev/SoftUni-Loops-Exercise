5cd6d00ec023fc1a8bcc50af2650a07d5dd0031d
V-Gulev <velizargulev@abv.bg>
2024-08-12 20:57:47 +0300

    Rage Expenses

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -3,19 +3,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double availableMoney = Double.parseDouble(sc.nextLine());
-        int studentCount = Integer.parseInt(sc.nextLine());
-        double lightsaberPrice = Double.parseDouble(sc.nextLine());
-        double robePrice = Double.parseDouble(sc.nextLine());
-        double beltPrice = Double.parseDouble(sc.nextLine());
-        int freeBelt = studentCount / 6;
-        int sabersNeeded = (int) Math.ceil(studentCount * 1.1);
-        double cost = sabersNeeded * lightsaberPrice + (studentCount * robePrice) + ((studentCount - freeBelt) * beltPrice);
-        double neededMoney = cost - availableMoney;
-        if (cost <= availableMoney) {
-            System.out.printf("The money is enough - it would cost %.2flv.", cost);
-        } else System.out.printf("George Lucas will need %.2flv more.", neededMoney);
-
+        int lostGames = Integer.parseInt(sc.nextLine());
+        double headsetPrice = Double.parseDouble(sc.nextLine());
+        double mousePrice = Double.parseDouble(sc.nextLine());
+        double keyboardPrice = Double.parseDouble(sc.nextLine());
+        double displayPrice = Double.parseDouble(sc.nextLine());
+        double headsets = 0;
+        double mouses = 0;
+        double keyboards = 0;
+        double display = 0;
+        for (int i = 2; i <= lostGames; i += 2) {
+            headsets++;
+        }
+        for (int i = 3; i <= lostGames; i += 3) {
+            mouses++;
+        }
+        for (int i = 6; i <=lostGames ; i+=6) {
+            keyboards++;
+        }
+        for (int i = 12; i <=lostGames ; i+=12) {
+            display++;
+        }
+        double totalCost=display*displayPrice+headsetPrice*headsets+keyboardPrice*keyboards+mousePrice*mouses;
+        System.out.printf("Rage expenses: %.2f lv.",totalCost);
 
     }
 }
