1a4da0b7bd4685bf8e9314cce9fa9d7346332ba3
V-Gulev <velizargulev@abv.bg>
2024-08-12 20:51:15 +0300

    Strong Number

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -3,26 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String username = sc.nextLine();
-        String reversed = "";
-        int tries = 0;
-        for (int i = username.length() - 1; i >= 0; i--) {
-            reversed += username.charAt(i);
-        }
-        while (true) {
-            String password = sc.nextLine();
-            if (tries==3){
-                System.out.printf("User %s blocked!",username);
-                break;
+        int number = Integer.parseInt(sc.nextLine());
+        int sum = 1;
+        int startingNumber=number;
+        int totalSum = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            for (int i = 1; i <= digit; i++) {
+                sum = sum * i;
             }
-            if (password.equals(reversed)) {
-                System.out.printf("User %s logged in.", username);
-                break;
-            } else {
-                System.out.println("Incorrect password. Try again.");
-                tries++;
-            }
+            totalSum += sum;
+            number /= 10;
+            sum=1;
         }
+        if (totalSum == startingNumber) {
+            System.out.println("yes");
+        } else System.out.println("no");
     }
 }
 
