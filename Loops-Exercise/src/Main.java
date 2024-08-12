90e09c6d78b92cc0f741efd5064072a62e7d8c65
V-Gulev <velizargulev@abv.bg>
2024-08-12 20:55:10 +0300

    Orders

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number = Integer.parseInt(sc.nextLine());
-        for (int i = 1; i <=number ; i++) {
-            for (int j = 1; j <=i ; j++) {
-                System.out.print(i+" ");
-            }
+        int N = Integer.parseInt(sc.nextLine());
+        double price;
+        double totalPrice = 0;
+        for (int i = 0; i < N; i++) {
+            double pricePerCapsule = Double.parseDouble(sc.nextLine());
+            int daysInMonth = Integer.parseInt(sc.nextLine());
+            int capsuleCount = Integer.parseInt(sc.nextLine());
+            price = ((daysInMonth * capsuleCount) * pricePerCapsule);
+            totalPrice += price;
+            System.out.printf("The price for the coffee is: $%.2f", price);
             System.out.println();
         }
-
-
+        System.out.printf("Total: $%.2f", totalPrice);
 
     }
 }
