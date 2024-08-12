f120e97e22e199acc6269fe5e86ad876ae89f860
V-Gulev <velizargulev@abv.bg>
2024-08-12 20:40:01 +0300

    Refactor Sum of Odd

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            int number = sc.nextInt();
-
-            if (number % 2 == 0) {
-                System.out.println("The number is: " + Math.abs(number));
-                break;
-            } else {
-                System.out.println("Please write an even number.");
-            }
+        int n = Integer.parseInt(sc.nextLine());
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.println((2 * i) + 1);
+            sum += 2*i+1;
         }
+        System.out.printf("Sum: %d%n", sum);
 
 
     }
