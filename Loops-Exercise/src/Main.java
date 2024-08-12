c9530bf31b53cb8b8664f193744ff9f82fe1bb83
V-Gulev <velizargulev@abv.bg>
2024-08-12 20:50:15 +0300

    Login

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -3,16 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number1 = Integer.parseInt(sc.nextLine());
-        int number2 = Integer.parseInt(sc.nextLine());
-        int sum = 0;
-        for (int i = number1; i <= number2; i++) {
-            System.out.print(i + " ");
-            sum += i;
+        String username = sc.nextLine();
+        String reversed = "";
+        int tries = 0;
+        for (int i = username.length() - 1; i >= 0; i--) {
+            reversed += username.charAt(i);
         }
-        System.out.println();
-        System.out.println("Sum: " + sum);
-
+        while (true) {
+            String password = sc.nextLine();
+            if (tries==3){
+                System.out.printf("User %s blocked!",username);
+                break;
+            }
+            if (password.equals(reversed)) {
+                System.out.printf("User %s logged in.", username);
+                break;
+            } else {
+                System.out.println("Incorrect password. Try again.");
+                tries++;
+            }
+        }
     }
 }
 
