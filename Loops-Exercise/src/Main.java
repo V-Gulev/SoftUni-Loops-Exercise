047efa5948d7a05d17b88505f28c97a28ac630b1
V-Gulev <velizargulev@abv.bg>
2024-08-12 20:36:37 +0300

    Multiplication table

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -4,12 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        int sum = 0;
-        for (int i = 1; i <n*2 ; i+=2) {
-            System.out.println(i);
-            sum+=i;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " X " + i + " = " + (n * i));
+
         }
-        System.out.println("Sum: "+sum);
 
     }
 }
