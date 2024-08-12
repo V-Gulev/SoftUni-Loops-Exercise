0b4cee4a2c19d7bc3659a0c1b46dcbe3db007b55
V-Gulev <velizargulev@abv.bg>
2024-08-12 20:02:15 +0300

    Passed

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scannerInput = new Scanner(System.in);
-        String name = scannerInput.nextLine();
-        int age = Integer.parseInt(scannerInput.nextLine());
         double grade = Double.parseDouble(scannerInput.nextLine());
-        System.out.printf("Name: %s, Age: %d, Grade: %.2f",name,age,grade);
+        if (grade>=3.00){
+            System.out.println("Passed!");
+        }
     }
 }
