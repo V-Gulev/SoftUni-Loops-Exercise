06f6b5b743de88833e9eeaf5285e8528cf80098e
V-Gulev <velizargulev@abv.bg>
2024-08-12 20:34:10 +0300

    Divisible by 3

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -1,40 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String day = sc.nextLine();
-        double age = Double.parseDouble(sc.nextLine());
-        switch (day) {
-            case "Weekday":
-                if (age >= 0 && age <= 18) {
-                    System.out.println("12$");
-                } else if (age <= 64 && age>=0) {
-                    System.out.println("18$");
-                } else if (age <= 122 && age>=0) {
-                    System.out.println("12$");
-                } else System.out.println("Error!");
-                break;
-            case "Weekend":
-                if (age >= 0 && age <= 18) {
-                    System.out.println("15$");
-                } else if (age <= 64 && age>=0) {
-                    System.out.println("20$");
-                } else if (age <= 122 && age>=0) {
-                    System.out.println("15$");
-                } else System.out.println("Error!");
-                break;
-            case "Holiday":
-                if (age >= 0 && age <= 18) {
-                    System.out.println("5$");
-                } else if (age <= 64 && age >= 0) {
-                    System.out.println("12$");
-                } else if (age <= 122 && age>=0) {
-                    System.out.println("10$");
-                } else System.out.println("Error!");
-                break;
+        for (int i = 3; i <=100 ; i+=3) {
+            System.out.println(i);
         }
-
 
     }
 }
