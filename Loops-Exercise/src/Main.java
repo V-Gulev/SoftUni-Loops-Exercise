e5611ff62bb875c5d913cc77f82cd3eb2849bd13
V-Gulev <velizargulev@abv.bg>
2024-08-12 20:29:28 +0300

    Theatre Promotion

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -3,17 +3,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String country= sc.nextLine();
-        switch (country) {
-            case "England","USA":
-                System.out.println("English");
+        String day = sc.nextLine();
+        double age = Double.parseDouble(sc.nextLine());
+        switch (day) {
+            case "Weekday":
+                if (age >= 0 && age <= 18) {
+                    System.out.println("12$");
+                } else if (age <= 64 && age>=0) {
+                    System.out.println("18$");
+                } else if (age <= 122 && age>=0) {
+                    System.out.println("12$");
+                } else System.out.println("Error!");
                 break;
-            case "Spain","Argentina","Mexico":
-                System.out.println("Spanish");
+            case "Weekend":
+                if (age >= 0 && age <= 18) {
+                    System.out.println("15$");
+                } else if (age <= 64 && age>=0) {
+                    System.out.println("20$");
+                } else if (age <= 122 && age>=0) {
+                    System.out.println("15$");
+                } else System.out.println("Error!");
                 break;
-            default:
-                System.out.println("unknown");
+            case "Holiday":
+                if (age >= 0 && age <= 18) {
+                    System.out.println("5$");
+                } else if (age <= 64 && age >= 0) {
+                    System.out.println("12$");
+                } else if (age <= 122 && age>=0) {
+                    System.out.println("10$");
+                } else System.out.println("Error!");
                 break;
         }
+
+
     }
 }
+
+
