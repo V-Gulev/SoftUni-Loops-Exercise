1af4ed533140bb2161d927a8dd645efb33620907
V-Gulev <velizargulev@abv.bg>
2024-08-13 19:37:45 +0300

    Gaming Store

--- Loops-Exercise/src/Main.java
+++ Loops-Exercise/src/Main.java
@@ -2,44 +2,81 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scannerInput = new Scanner(System.in);
-        int n1 = Integer.parseInt(scannerInput.nextLine());
-        int lastDigit = n1 % 10;
-        switch (lastDigit) {
-            case 0:
-                System.out.println("zero");
-                break;
-            case 1:
-                System.out.println("one");
-                break;
-            case 2:
-                System.out.println("two");
-                break;
-            case 3:
-                System.out.println("three");
-                break;
-            case 4:
-                System.out.println("four");
-                break;
-            case 5:
-                System.out.println("five");
-                break;
-            case 6:
-                System.out.println("six");
-                break;
-            case 7:
-                System.out.println("seven");
-                break;
-            case 8:
-                System.out.println("eight");
-                break;
-            case 9:
-                System.out.println("nine");
-                break;
-            default:
-                System.out.println("unexpected case");
-                break;
+        Scanner sc = new Scanner(System.in);
+        double budget = Double.parseDouble(sc.nextLine());
+        double spentMoney = 0;
 
+        while (true) {
+            String gameName = sc.nextLine();
+
+            if (gameName.equals("Game Time") && budget>0) {
+                System.out.printf("Total spent: $%.2f. Remaining: $%.2f%n", spentMoney, budget);
+                break;
+            }else if (gameName.equals("Game Time")) break;
+
+            switch (gameName) {
+                case "OutFall 4":
+                    if (budget < 39.99) {
+                        System.out.println("Too Expensive");
+                    } else {
+                        budget -= 39.99;
+                        spentMoney += 39.99;
+                        System.out.println("Bought OutFall 4");
+                    }
+                    break;
+                case "CS: OG":
+                    if (budget < 15.99) {
+                        System.out.println("Too Expensive");
+                    } else {
+                        budget -= 15.99;
+                        spentMoney += 15.99;
+                        System.out.println("Bought CS: OG");
+                    }
+                    break;
+                case "Zplinter Zell":
+                    if (budget < 19.99) {
+                        System.out.println("Too Expensive");
+                    } else {
+                        budget -= 19.99;
+                        spentMoney += 19.99;
+                        System.out.println("Bought Zplinter Zell");
+                    }
+                    break;
+                case "Honored 2":
+                    if (budget < 59.99) {
+                        System.out.println("Too Expensive");
+                    } else {
+                        budget -= 59.99;
+                        spentMoney += 59.99;
+                        System.out.println("Bought Honored 2");
+                    }
+                    break;
+                case "RoverWatch":
+                    if (budget < 29.99) {
+                        System.out.println("Too Expensive");
+                    } else {
+                        budget -= 29.99;
+                        spentMoney += 29.99;
+                        System.out.println("Bought RoverWatch");
+                    }
+                    break;
+                case "RoverWatch Origins Edition":
+                    if (budget < 39.99) {
+                        System.out.println("Too Expensive");
+                    } else {
+                        budget -= 39.99;
+                        spentMoney += 39.99;
+                        System.out.println("Bought RoverWatch Origins Edition");
+                    }
+                    break;
+                default:
+                    System.out.println("Not Found");
+                    break;
+            }
+
+            if (budget == 0) {
+                System.out.println("Out of money!");
+            }
         }
     }
 }
